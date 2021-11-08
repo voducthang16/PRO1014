2c4281d940696962bbb832b34d8161f7c1c38f2e
Bach <bach19112002@gmail.com>
2021-11-08 21:44:03 +0700

    update pagenotfound

--- mvc/views/pages/pagenotfound.php
+++ mvc/views/pages/pagenotfound.php
@@ -1,1 +1,44 @@
-<h3>404</h3>
+<div class="container error-404">
+    <div class="row">
+        <div class="error__content">
+            <img class="error__content-img" src="<?= BASE_URL ?>public/assets/img/404.png" width="300" alt="404 Error">
+            <h2 class="error__content-heading">404 error</h2>
+            <h5 class="error__content-text">We can't seem to find the page you are looking for.</h5>
+            <a href="#!" class="error__content-link">Here are some helpful links instead:</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col mb-16">
+            <a class="card" href="#!">
+                <div class="card-body">
+                    <i class="fal fa-home"></i>
+                    <div class="card-body--info">
+                        <h5>Homepage</h5>
+                        <span>Return to homepage</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col mb-16">
+            <a class="card" href="#!">
+                <div class="card-body">
+                    <i class="fal fa-search"></i>
+                    <div class="card-body--info">
+                        <h5>Search</h5>
+                        <span>Find with advanced search</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col mb-16"><a class="card" href="#!">
+                <div class="card-body">
+                    <i class="fal fa-life-ring"></i>
+                    <div class="card-body--info">
+                        <h5>Help &amp; Support</h5>
+                        <span>Visit our help center</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
