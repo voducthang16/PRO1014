85afdb39cc355d0be462e96d277387049a614368
Bach <bach19112002@gmail.com>
2021-11-08 19:20:40 +0700

    update footer

--- mvc/views/pages/footer.php
+++ mvc/views/pages/footer.php
@@ -1,1 +1,164 @@
-THIS IS FOOTER
+<footer class="footer">
+  <div class="footer__top">
+    <div class="container wide">
+      <div class="footer__content row">
+        <div class="col l-4">
+          <h3 class="footer__heading">Shop departments</h3>
+          <ul class="footer__list">
+            <li class="footer__items"><a href="#!" class="footer__link">Sneakers & Athletic</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Athletic Apparel</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Sandals</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Jeans</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Shirts & Tops</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Shorts</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">T-Shirts</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Swimwear</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Clogs & Mules</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Bags & Wallets</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Accessories</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link"> Sunglasses & Eyewear</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Watches</a></li>
+          </ul>
+        </div>
+        <div class="col l-4">
+          <h3 class="footer__heading">Account & shipping info</h3>
+          <ul class="footer__list">
+            <li class="footer__items"><a href="#!" class="footer__link">Your account</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Shipping rates & policies</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Refunds & replacements</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Order tracking</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Delivery info</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Taxes & fees</a></li>
+          </ul>
+          <h3 class="footer__heading">About us</h3>
+          <ul class="footer__list">
+            <li class="footer__items"><a href="#!" class="footer__link">About company</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Our team</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">Careers</a></li>
+            <li class="footer__items"><a href="#!" class="footer__link">News</a></li>
+          </ul>
+        </div>
+        <div class="col l-4">
+          <div class="footer__box">
+            <div class="footer__box-top">
+              <h3 class="footer__heading">Stay informed</h3>
+              <form class="subscription-form" action="" method="post" name="ml-embedded-subscribe-form" target="">
+                <div class="input-group">
+                  <i class="fal fa-envelope input-group__icon"></i>
+                  <input class="form-control rounded-start" type="email" name="EMAIL" placeholder="Your email" required>
+                  <button class="btn btn-primary btn-subscription-form" type="submit" name="subscribe">Subscribe*</button>
+                </div>
+              </form>
+              <p>*Subscribe to our newsletter to receive early discount offers, updates and new products info.</p>
+            </div>
+            <div class="footer__box-bot">
+              <h3 class="footer__heading">Download our app</h3>
+              <div class="footer__app-download">
+                <a href="#!">
+                  <img src="<?= BASE_URL ?>public/assets/img/gg_play.png" class="gg-play-store" alt="google-play-store">
+                </a>
+                <a href="#!">
+                  <img src="<?= BASE_URL ?>public/assets/img/app_store.png" class="app-store" alt="app-store">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer__bottom">
+    <div class="container wide">
+      <div class="footer__services row">
+        <div class="col l-3 footer__services-item">
+          <i class="fal fa-rocket-launch footer__services-icon"></i>
+          <div>
+            <h6 class="footer__services-heading">Fast and free delivery</h6>
+            <p class="footer__services-text">Free delivery for all orders over $200</p>
+          </div>
+        </div>
+        <div class="col l-3 footer__services-item">
+          <i class="fal fa-hand-holding-usd footer__services-icon"></i>
+          <div>
+            <h6 class="footer__services-heading">Money back guarantee</h6>
+            <p class="footer__services-text">We return money within 30 days</p>
+          </div>
+        </div>
+        <div class="col l-3 footer__services-item">
+          <i class="fal fa-headset footer__services-icon"></i>
+          <div>
+            <h6 class="footer__services-heading">24/7 customer support</h6>
+            <p class="footer__services-text">Friendly 24/7 customer support</p>
+          </div>
+        </div>
+        <div class="col l-3 footer__services-item">
+          <i class="fal fa-credit-card footer__services-icon"></i>
+          <div>
+            <h6 class="footer__services-heading">Secure online payment</h6>
+            <p class="footer__services-text">We possess SSL / Secure сertificate</p>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="row footer__services-info">
+        <div class="col l-6">
+          <div class="box-1">
+            <div class="navbar-logo footer-logo">
+              <a href="#" class="navbar-logo-link">
+                <img width="100%" src="<?= BASE_URL ?>public/assets/img/test.png" alt="LOGO">
+              </a>
+            </div>
+            <div class="currency-exchange">
+              <img src="<?= BASE_URL ?>public/assets/img/flags/US.png" alt="">
+              <span>Eng / $</span>
+              <i class="fad fa-caret-down"></i>
+            </div>
+          </div>
+          <div class="box-2 l-9">
+            <ul class="footer__list-row">
+              <li class="footer__items"><a href="#!" class="footer__link">Outlets </a></li>
+              <li class="footer__items"><a href="#!" class="footer__link">Affiliates</a></li>
+              <li class="footer__items"><a href="#!" class="footer__link">Support</a></li>
+              <li class="footer__items"><a href="#!" class="footer__link">Privacy</a></li>
+              <li class="footer__items"><a href="#!" class="footer__link">Terms of use</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col l-6">
+          <div class="icon-bar">
+            <div class="icon facebook">
+              <div class="tooltip">Facebook</div>
+              <span><i class="fab fa-facebook-f"></i></span>
+            </div>
+            <div class="icon twitter">
+              <div class="tooltip">Twitter</div>
+              <span><i class="fab fa-twitter"></i></span>
+            </div>
+            <div class="icon instagram">
+              <div class="tooltip">Instagram</div>
+              <span><i class="fab fa-instagram"></i></span>
+            </div>
+            <div class="icon pinterest">
+              <div class="tooltip">Pinterest</div>
+              <span><i class="fab fa-pinterest"></i></span>
+            </div>
+            <div class="icon youtube">
+              <div class="tooltip">Youtube</div>
+              <span><i class="fab fa-youtube"></i></span>
+            </div>
+          </div>
+          <div class="pay-icon-bar">
+            <i class="fab fa-cc-visa"></i>
+            <i class="fab fa-cc-amex"></i>
+            <i class="fab fa-cc-mastercard"></i>
+            <i class="fab fa-cc-discover"></i>
+          </div>
+        </div>
+      </div>
+      <div class="copyright">
+        <span>© All rights reserved. Made by Nhom 1-PRO1014</span>
+      </div>
+    </div>
+  </div>
+  </div>
+</footer>
