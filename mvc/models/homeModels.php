52fca509fc1b0653d251cbbfe11b9b71b41d482f
GB <voducthang16@gmail.com>
2021-11-18 17:21:16 +0700

    update query product homepage
    5:21PM
    11/18/2021

--- mvc/models/homeModels.php
+++ mvc/models/homeModels.php
@@ -1,9 +1,7 @@
 <?php
     class homeModels extends database {
         function getProducts() {
-            $query = "SELECT products.id, category_id, name, slug, thumbnail, products_type.price_origin, products_type.price_sale FROM `products` 
-            INNER JOIN products_type ON products.id = products_type.product_id 
-            WHERE status = 1 and category_id != 2 GROUP BY products_type.price_origin, products_type.price_sale ORDER BY id DESC LIMIT 8";
+            $query = "SELECT * FROM (SELECT products.id, category_id, name, slug, thumbnail, products_type.price_origin, products_type.price_sale, ROW_NUMBER() OVER(PARTITION BY name ORDER BY products_type.price_sale DESC) rn FROM `products` INNER JOIN products_type ON products.id = products_type.product_id WHERE status = 1 and category_id != 2 GROUP BY products_type.price_origin, products_type.price_sale, products.name) a WHERE rn = 1 ORDER BY id DESC LIMIT 8";
             $result = $this->connect->prepare($query);
             $result->execute();
             return $result->fetchAll();
