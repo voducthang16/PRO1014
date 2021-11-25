7d5906656a6495caa9036b6d70a833c37090d800
nguyennhieu1507 <nhieunxps16310@fpt.edu.vn>
2021-11-25 23:16:28 +0700

    base url

--- mvc/core/config.php
+++ mvc/core/config.php
@@ -3,7 +3,7 @@
     define("DB_USER", "root");
     define("DB_PASS", "");
     define("DB_NAME", "pro1014");
-    define("BASE_URL", "http://");
+    define("BASE_URL", "http://localhost/PRO1014");
     define('APP_ID', '577984519968283');
     define('APP_SECRET', '41072c378f47524a475565e52036aa1e');
     define('API_VERSION', 'v3.0');
