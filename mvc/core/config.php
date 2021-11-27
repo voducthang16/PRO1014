9b27caec03fb27e59537187cac0af6e65eefdbaf
nguyennhieu1507 <nhieunxps16310@fpt.edu.vn>
2021-11-27 17:31:09 +0700

    push tạm code log google

--- mvc/core/config.php
+++ mvc/core/config.php
@@ -6,6 +6,8 @@
     define("BASE_URL", "http://localhost/PRO1014/");
     define('APP_ID', '435794041268676');
     define('APP_SECRET', '45f3fbe9d376c24081856c128262c8f4');
+    define('APP_SECRET_GOOGLE', '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');
+    define('APP_ID_GOOGLE', '849441706706-m9jbb0uh9blcc21mbr15om7vj2lt1v58.apps.googleusercontent.com');
     // define("DB_HOST", "localhost");
     // define("DB_USER", "fpolyga");
     // define("DB_PASS", "*8KA6Z@t5tj3vE");
--- mvc/views/pages/sign.php
+++ mvc/views/pages/sign.php
@@ -22,12 +22,24 @@ if (isset($_SESSION["member-login"]) && $_SESSION["member-login"] == "true") {
                             <a href="<?=BASE_URL?>sign/forgotpassword" class="forgot-pass">Quên mật khẩu ?</a>
                         </div>
                         <div class="form-group" style="text-align: left">
-                            <a href="https://www.facebook.com/v12.0/dialog/oauth?client_id=435794041268676&redirect_uri=<?php echo BASE_URL; ?>/sign/loginFB&scope=public_profile" class="forgot-pass">Đăng nhập bằng facebook</a>
+                            <a href="https://www.facebook.com/v12.0/dialog/oauth?client_id=435794041268676&redirect_uri=<?php BASE_URL; ?>sign/loginFB&scope=public_profile" class="forgot-pass">Đăng nhập bằng facebook</a>
+                        </div>
+                        <div class="form-group" style="text-align: right">
+                            <a href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=<?= APP_ID_GOOGLE ?>&redirect_uri=<?=BASE_URL?>sign/loginGoogle&scope=https%3A%2F%2Fmail.google.com&approval_prompt=force" class="forgot-pass">Đăng nhập google</a>
                         </div>
                         <div style="text-align: right" class="form-group">
                             <button type="submit" class="btn btn-primary">Đăng nhập</button>
                         </div>
                     </form>
+                    <script>
+                        function onSignIn(googleUser) {
+                            var profile = googleUser.getBasicProfile();
+                            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                            console.log('Name: ' + profile.getName());
+                            console.log('Image URL: ' + profile.getImageUrl());
+                            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+                        }
+                    </script>
                 </div>
             </div>
             <div class="col l-6">
