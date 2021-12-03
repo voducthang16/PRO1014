7b07f28c5fafbc8deeddf9bfc19a0ca82aafdd2d
Bach <bach19112002@gmail.com>
2021-12-03 14:50:42 +0700

    update about us
    them trang mới (aboutus)

---
+++ mvc/views/pages/aboutus.php
@@ -0,0 +1,177 @@
+<div class="aboutus">
+    <div class="page-header page-header__aboutus">
+        <div class="container wide">
+            <div class="page-wrapper page-wrapper__aboutus">
+                <h2 class="page-title page-title__aboutus">Giới thiệu</h2>
+                <nav class="page-navbar">
+                    <ul class="page-list">
+                        <li class="page-item">
+                            <a href="<?= BASE_URL ?>" class="page-link page-link__aboutus"><i class="fal fa-home"></i>Trang chủ<i class="fal fa-chevron-right"></i></a>
+                        </li>
+                        <li class="page-item">
+                            <a class="page-link page-link__aboutus">Giới thiệu</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <div class="row aboutus-box-1">
+        <div class="l-6">
+            <img class="" src="<?= BASE_URL ?>/public/assets/img/aboutus/01.jpg" alt="01-aboutus">
+        </div>
+        <div class="l-6 aboutus__content">
+            <div class="aboutus__content-box">
+                <h2>Search, Select, Buy online</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
+                <a class="btn btn-primary btn-shadow red" href="shop-grid-ls.html">View products</a>
+            </div>
+        </div>
+    </div>
+    <div class="row aboutus-box-2">
+        <div class="l-6 aboutus__content">
+            <div class="aboutus__content-box">
+                <h2>Fast delivery worldwide</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
+                <a class="btn btn-primary btn-shadow blue" href="shop-grid-ls.html">Shipping details</a>
+            </div>
+        </div>
+        <div class="l-6">
+            <img class="" src="<?= BASE_URL ?>/public/assets/img/aboutus/02.jpg" alt="02-aboutus">
+        </div>
+    </div>
+    <div class="row aboutus-box-3">
+        <div class="l-6">
+            <img class="" src="<?= BASE_URL ?>/public/assets/img/aboutus/03.jpg" alt="03-aboutus">
+        </div>
+        <div class="l-6 aboutus__content">
+            <div class="aboutus__content-box">
+                <h2>Great mobile app. Shop on the go</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
+                <div class="footer__app-download">
+                    <a href="#!" style="padding: 0;">
+                        <img src="public/assets/img/gg_play.png" class="gg-play-store" alt="google-play-store">
+                    </a>
+                    <a href="#!" style="padding: 0;">
+                        <img src="public/assets/img/app_store.png" class="app-store" alt="app-store">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row aboutus-box-4" style="border-bottom: 1px solid #EAEAEA;">
+        <div class="l-6 aboutus__content">
+            <div class="aboutus__content-box">
+                <h2>Shop offline. Cozy outlet stores</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
+                <a class="btn btn-primary btn-shadow yellow" href="shop-grid-ls.html">Shipping details</a>
+            </div>
+        </div>
+        <div class="l-6">
+            <img class="" src="<?= BASE_URL ?>/public/assets/img/aboutus/04.jpg" alt="04-aboutus">
+        </div>
+    </div>
+    <div class="container core-team wide aboutus-box-5">
+        <h2 class="core-team__heading">Our core team</h2>
+        <p class="core-team__text">People behind your great shopping experience</p>
+        <div class="row">
+            <div class="l-4">
+                <div class="core-team__box">
+                    <img class="core-team__img" src="<?= BASE_URL ?>/public/assets/img/team/01.jpg" width="96" alt="bachking">
+                    <div class="core-team__item">
+                        <h6>Bách Nguyễn</h6>
+                        <p>CEO, Co-founder</p>
+                        <a class="" href="mailto:bach@example.com">
+                            <i class="fal fa-envelope" style="margin: -2px 8px 0px 0px;"></i>bach@example.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="l-4">
+                <div class="core-team__box">
+                    <img class="core-team__img" src="<?= BASE_URL ?>/public/assets/img/team/06.jpg" width="96" alt="thanhxuan">
+                    <div class="core-team__item">
+                        <h6>Thanh Xuân</h6>
+                        <p>Chief of Marketing</p>
+                        <a class="" href="mailto:thanhxuan@example.com">
+                            <i class="fal fa-envelope" style="margin: -2px 8px 0px 0px;"></i>thanhxuan@example.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="l-4">
+                <div class="core-team__box">
+                    <img class="core-team__img" src="<?= BASE_URL ?>/public/assets/img/team/04.jpg" width="96" alt="tuankiet">
+                    <div class="core-team__item">
+                        <h6>Tuấn Kiệt</h6>
+                        <p>Financial director</p>
+                        <a class="" href="mailto:kiet@example.com">
+                            <i class="fal fa-envelope" style="margin: -2px 8px 0px 0px;"></i>kiet@example.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="l-4">
+                <div class="core-team__box">
+                    <img class="core-team__img" src="<?= BASE_URL ?>/public/assets/img/team/02.jpg" width="96" alt="ducthang">
+                    <div class="core-team__item">
+                        <h6>Đức Thắng</h6>
+                        <p>Lead UX designer</p>
+                        <a class="" href="mailto:thang@example.com">
+                            <i class="fal fa-envelope" style="margin: -2px 8px 0px 0px;"></i>thang@example.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="l-4">
+                <div class="core-team__box">
+                    <img class="core-team__img" src="<?= BASE_URL ?>/public/assets/img/team/05.jpg" width="96" alt="xuannhieu">
+                    <div class="core-team__item">
+                        <h6>Xuân Nhiều</h6>
+                        <p>Website development</p>
+                        <a class="" href="mailto:nhieu@example.com">
+                            <i class="fal fa-envelope" style="margin: -2px 8px 0px 0px;"></i>nhieu@example.com</a>
+                    </div>
+                </div>
+            </div>
+            <div class="l-4">
+                <div class="core-team__box">
+                    <img class="core-team__img" src="<?= BASE_URL ?>/public/assets/img/team/03.jpg" width="96" alt="huy">
+                    <div class="core-team__item">
+                        <h6>Quang Huy</h6>
+                        <p>Content manager</p>
+                        <a class="" href="mailto:huy@example.com">
+                            <i class="fal fa-envelope" style="margin: -2px 8px 0px 0px;"></i>huy@example.com</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="row aboutus-box-6" style="border-top: 1px solid #EAEAEA;">
+        <div class="l-6 aboutus__content-form">
+            <div class="aboutus-form">
+                <h2>We are hiring new talents</h2>
+                <p class="">If you want to be part of our team please submit you CV using the form below:</p>
+                <form class="row" method="post">
+                    <div class="l-6 pd-mt">
+                        <input class="form-control" type="text" placeholder="Your name" required>
+                    </div>
+                    <div class="l-6 pd-mt">
+                        <input class="form-control" type="email" placeholder="Your email" required>
+                    </div>
+                    <div class="l-12 pd-mt">
+                        <textarea class="form-control" rows="4" placeholder="Message" required></textarea>
+                    </div>
+                    <div class="l-12 pd-mt">
+                        <input class="form-control" type="file" required>
+                    </div>
+                    <div class="l-12 pd-mt">
+                        <button class="btn btn-info btn-shadow blue-cyan" type="submit">Submit your CV</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="l-6">
+            <img class="" src="<?= BASE_URL ?>/public/assets/img/aboutus/05.jpg" alt="05-aboutus">
+        </div>
+    </div>
+</div>
