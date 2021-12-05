f5710736c89c861be3f583a2bbdcbf3784b1d088
Bach <bach19112002@gmail.com>
2021-12-05 16:11:41 +0700

    update blog aboutus contact vietsup

--- mvc/views/pages/aboutus.php
+++ mvc/views/pages/aboutus.php
@@ -23,18 +23,18 @@
         </div>
         <div class="l-6 aboutus__content">
             <div class="aboutus__content-box">
-                <h2>Search, Select, Buy online</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
-                <a class="btn btn-primary btn-shadow red" href="shop-grid-ls.html">View products</a>
+                <h2>Tìm kiếm, Chọn, Mua trực tuyến</h2>
+                <p>Kính gửi quý khách hàng/đối tác, Thời gian gần đây, nhận được thông tin về việc một số tổ chức/ cá nhân lợi dụng việc các doanh nghiệp gặp khó khăn cần vay vốn để duy trì hoạt động kinh doanh, đã bằng thủ đoạn tinh vi mạo danh Ngân Lượng và lợi dụng uy tín của công ty để lừa gạt khách hàng doanh nghiệp vay vốn với lãi suất thấp (Chỉ 0,7%/ tháng, tương đương với 8,4%/năm)...</p>
+                <a class="btn btn-primary btn-shadow red" href="shop-grid-ls.html">Xem thêm</a>
             </div>
         </div>
     </div>
     <div class="container row no-gutters aboutus-box-2">
         <div class="l-6 aboutus__content">
             <div class="aboutus__content-box">
-                <h2>Fast delivery worldwide</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
-                <a class="btn btn-primary btn-shadow blue" href="shop-grid-ls.html">Shipping details</a>
+                <h2>Giao hàng nhanh chóng</h2>
+                <p>Giao Hàng Nhanh bắt đầu hoạt động từ năm 2012. Đây là đơn vị vận chuyển hàng hóa chuyên nghiệp, rất mạnh về dịch vụ ship hàng thu tiền hộ (ship COD) và phát triển với đội ngũ nhân viên đông đảo.Điểm đặc biệt của Giao Hàng Nhanh đó là dịch vụ ship hàng 60 phút. Với dịch vụ này, khách hàng có thể nhận được hàng trong vòng 60 phút kể từ khi nhân viên lấy hàng. Khoảng giá cho dịch vụ giao hàng của giao hàng nhanh uy tín dao động từ 20.000 đến 90.000 đồng...</p>
+                <a class="btn btn-primary btn-shadow blue" href="shop-grid-ls.html">Xem Thêm</a>
             </div>
         </div>
         <div class="l-6">
@@ -47,8 +47,8 @@
         </div>
         <div class="l-6 aboutus__content">
             <div class="aboutus__content-box">
-                <h2>Great mobile app. Shop on the go</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
+                <h2>Tải ứng dụng trên App Store,Google Play</h2>
+                <p>Từ giao diện, tiện ích, năng suất của ứng dụng được tạo ra đều nhằm hướng đến các nhu cầu: giải trí, thể thao,....hay bất kỳ thứ gì khác cần thiết trong cuộc sống. Đặc biệt là mua sắm, có đến 89% người dùng di động ở Việt Nam được hỏi nói rằng đã từng thực hiện tất cả các bước để mua hàng trong ứng dụng. Những người trong độ tuổi 18-24 là nhóm sẵn sàng mua qua ứng dụng nhất (94%) - vnexpress.net.Bên cạnh phát triển app mobile, người ta còn thiết kế cả mobile web tùy theo mục đích sử dụng. Tuy nhiên, so với trang web thì ứng dụng di động có thể cung cấp nhiều tương tác hơn, trình bày những thông tin cụ thể ở định dạng thân thiện và trực quan trên điện thoại.</p>
                 <div class="footer__app-download">
                     <a href="#!" style="padding: 0;">
                         <img src="public/assets/img/gg_play.png" class="gg-play-store" alt="google-play-store">
@@ -63,9 +63,9 @@
     <div class="container row no-gutters aboutus-box-4" style="border-bottom: 1px solid #EAEAEA;">
         <div class="l-6 aboutus__content">
             <div class="aboutus__content-box">
-                <h2>Shop offline. Cozy outlet stores</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id purus at risus pellentesque faucibus a quis eros. In eu fermentum leo. Integer ut eros lacus. Proin ut accumsan leo. Morbi vitae est eget dolor consequat aliquam eget quis dolor. Mauris rutrum fermentum erat, at euismod lorem pharetra nec. Duis erat lectus, ultrices euismod sagittis at, pharetra eu nisl. Phasellus id ante at velit tincidunt hendrerit. Aenean dolor dolor tristique nec. Tristique nulla aliquet enim tortor at auctor urna nunc. Sit amet aliquam id diam maecenas ultricies mi eget.</p>
-                <a class="btn btn-primary btn-shadow yellow" href="shop-grid-ls.html">Shipping details</a>
+                <h2>Mua trực tiếp tại cửa hàng</h2>
+                <p>Mặc dù đã có thể mua sắm trực tuyến mọi lúc mọi nơi, nhưng người tiêu dùng Mỹ vẫn bị thu hút bởi hàng loạt lời đề nghị hấp dẫn từ các hãng bán lẻ trong ngày Cyber Monday năm nay. Amazon đã tung ra một chương trình giảm giá kéo dài đến ngày 1/12, nhưng các hãng bán lẻ truyền thống lớn khác như Macy's, Best Buy cũng không chịu tỏ ra thua kém đối thủ với nhiều đề nghị hấp dẫn.Dự kiến, các hãng bán lẻ Mỹ có thể thu về 7,8 tỷ USD và tăng 18% so với cùng kỳ năm 2017 trong ngày Cyber Monday 2018 - được coi là ngày hội mua sắm trực tuyến lớn nhất trong lịch sử nước này.</p>
+                <a class="btn btn-primary btn-shadow yellow" href="shop-grid-ls.html">Xem thêm</a>
             </div>
         </div>
         <div class="l-6">
@@ -73,8 +73,8 @@
         </div>
     </div>
     <div class="container core-team wide aboutus-box-5">
-        <h2 class="core-team__heading">Our core team</h2>
-        <p class="core-team__text">People behind your great shopping experience</p>
+        <h2 class="core-team__heading">Ban điều hành</h2>
+        <p class="core-team__text">Những người đằng sau trải nghiệm mua sắm tuyệt vời của bạn</p>
         <div class="row">
             <div class="l-4">
                 <div class="core-team__box">
@@ -142,15 +142,13 @@
                     </div>
                 </div>
             </div>
-
-
         </div>
     </div>
     <div class="container row no-gutters aboutus-box-6" style="border-top: 1px solid #EAEAEA;">
         <div class="l-6 aboutus__content-form">
             <div class="aboutus-form">
-                <h2>We are hiring new talents</h2>
-                <p class="">If you want to be part of our team please submit you CV using the form below:</p>
+                <h2>Chúng tôi đang tuyển dụng những tài năng mới</h2>
+                <p class="">Nếu bạn muốn trở thành một phần của nhóm của chúng tôi, vui lòng gửi CV của bạn theo mẫu bên dưới:</p>
                 <form class="row" method="post">
                     <div class="l-6 pd-mt">
                         <input class="form-control" type="text" placeholder="Your name" required>
@@ -165,7 +163,7 @@
                         <input class="form-control" type="file" required>
                     </div>
                     <div class="l-12 pd-mt">
-                        <button class="btn btn-info btn-shadow blue-cyan" type="submit">Submit your CV</button>
+                        <button class="btn btn-info btn-shadow blue-cyan" type="submit">Nộp CV</button>
                     </div>
                 </form>
             </div>
